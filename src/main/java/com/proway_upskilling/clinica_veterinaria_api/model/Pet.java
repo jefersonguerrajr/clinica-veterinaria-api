45302dd0cf53c385792bc1f5938a4b3ff908d119
Tenorio <kamila.tenorio@capgemini.com>
2025-09-30 15:07:13 -0300

    feat: implementar CRUD no PetController e adicionar relacionamentos na entidade Pet

--- src/main/java/com/proway_upskilling/clinica_veterinaria_api/model/Pet.java
+++ src/main/java/com/proway_upskilling/clinica_veterinaria_api/model/Pet.java
@@ -7,7 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -20,12 +21,27 @@ public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
     private String especie;
+
+    @Column(nullable = false)
     private String raca;
+
+    @Column(nullable = false)
     private Double peso;
+
+    @Column(name = "data_nascimento", nullable = false)
     private LocalDate dataNascimento;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id", nullable = false)
     private Cliente dono;
-    //private List<Consultas> consultas;
+
+    @OneToMany(mappedBy = "pet", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Consulta> consultas = new ArrayList<>();
 
 }
