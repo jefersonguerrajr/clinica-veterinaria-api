5777bc765a042827463a15bb1ec7f85e0bec5bde
Jeferson Junior <jefersonguerrajr@live.com>
2025-10-09 16:35:14 -0300

    Ajuste README.md

--- clinica-veterinaria-api/src/main/java/com/proway_upskilling/clinica_veterinaria_api/config/SwaggerConfig.java
+++ clinica-veterinaria-api/src/main/java/com/proway_upskilling/clinica_veterinaria_api/config/SwaggerConfig.java
@@ -1,26 +1,40 @@
 package com.proway_upskilling.clinica_veterinaria_api.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Clínica Veterinária API",
+                version = "v1",
+                description = "Documentação da API da clínica veterinária com autenticação JWT",
+                license = @io.swagger.v3.oas.annotations.info.License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Clínica veterinária API")
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Clínica Veterinária API")
                         .version("v1")
-                        .description("Documentação  API Clínica veterinária")
-                        .termsOfService("https://example.com/terms")
-                        .license(new License()
+                        .description("Documentação da API da clínica veterinária com autenticação JWT")
+                        .license(new io.swagger.v3.oas.models.info.License()
                                 .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0"))
-                );
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
     }
-
 }
